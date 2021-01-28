a346922d698048c7907c145ea1a90861d392efb2
Burak Besli <admin_burak@arvatocrmturkey.com>
2021-01-28 18:52:37 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+            myDictionary.Add("Burak");
+            myDictionary.Add("Deniz");
+            myDictionary.Add("Bozca");
+
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
